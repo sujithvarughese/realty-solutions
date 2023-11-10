90206e0fb1778e592dafa0d6015e10287c4f0d9c
sujithv15 <64935098+sujithv15@users.noreply.github.com>
2023-11-09 20:36:00 -0500

    deploy updates

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ const app = express()
 dotenv.config();
 
 app.use(cors({
-	origin: ["http://localhost:5173"],
+	origin: ["http://localhost:5173", "https://realty-solutions.netlify.app"],
 	credentials: true
 }));
 
