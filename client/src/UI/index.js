c38302cb131adf253356481e22b336c673c82c8d
Sujith Varughese <sujithv15@gmail.com>
2023-11-16 14:47:43 -0500

    messages- changes previousMessages to array instead of object-> previous messages are fetched and added to the array (similar to linked list) so when a message is clicked, all previous messages in the thread are displayed

--- client/src/UI/index.js
+++
@@ -1,9 +0,0 @@
-import Backdrop from "./Backdrop.jsx";
-import Button from "./Button.jsx";
-import Card from "./Card.jsx"
-import Form from "./Form.jsx";
-import Input from "./Input.jsx";
-import InputSelect from "./InputSelect.jsx";
-import Modal from "./Modal.jsx"
-
-export { Backdrop, Card, Input, InputSelect, Button, Form, Modal }
