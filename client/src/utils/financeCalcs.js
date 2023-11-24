87a51bf7f04a2641dca7127b6934599b1f8ae293
Sujith Varughese <sujithv15@gmail.com>
2023-11-24 13:29:43 -0500

    media queries for mobile

--- client/src/utils/financeCalcs.js
+++ client/src/utils/financeCalcs.js
@@ -1,5 +1,5 @@
 
-// function to convert to dollar format when returning
+// function to convert to dollar format
 const convertToUSD = (number) => {
 	return  number.toLocaleString('en-US', {
 		style: 'currency',
