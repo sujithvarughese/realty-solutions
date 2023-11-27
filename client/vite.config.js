ecbb6dda348b850ad104450dd75396ac72ded78e
Sujith Varughese <sujithv15@gmail.com>
2023-11-27 15:04:32 -0500

    all accounting sections updated- user can edit directly from any tab, any amount of values as desired

--- client/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import 'dotenv/config'
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
