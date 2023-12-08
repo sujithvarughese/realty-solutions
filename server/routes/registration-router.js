9217d1112bb835293fb64a52016ddd3ce8e9c2d8
Sujith Varughese <sujithv15@gmail.com>
2023-12-08 16:35:45 -0500

    added feedback for user after successful message sent or unit created/updated

--- server/routes/registration-router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router()
-import { createRegistration, verifyRegistration } from "../controllers/registration-controller.js";
-import {authorizePermissions} from "../middleware/authentication.js";
-
-router.route("/create").post(authorizePermissions, createRegistration)
-router.route("/verify").post(verifyRegistration)
-
-export default router
