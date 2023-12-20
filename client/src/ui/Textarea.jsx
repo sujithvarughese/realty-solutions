cc5fecc11a3d157d515d4a361ba5b6429c3ab61f
Sujith Varughese <sujithv15@gmail.com>
2023-12-20 14:32:35 -0500

    textarea ui for form fixed

--- client/src/ui/Textarea.jsx
+++ client/src/ui/Textarea.jsx
@@ -1,12 +1,18 @@
 import styled from "styled-components";
 
 const Textarea = styled.textarea`
-  padding: 0.8rem 1.2rem;
+  padding: 0.5rem;
   border: 1px solid #bcccdc;
   border-radius: 5px;
+  height: 270px;
+  font: inherit;
+
   width: 100%;
   box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1),
   0 10px 10px -5px rgba(0, 0, 0, 0.04);
+  resize: none;
+  overflow:auto;
+  background-color: var(--COLOR-LIGHT);
   
   &:focus {
     outline: 4px auto -webkit-focus-ring-color;
